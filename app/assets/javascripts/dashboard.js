7f092cbf48b31de727661019ccaf2c91331845ba
unknown <agabdulin@gmail.com>
2015-10-24 22:29:07 +1300

    jquery dataTables added

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -3,3 +3,6 @@
 //= require 'dist/bootstrap-3.3.5.min'
 //= require sweet-alert
 //= require sweet-alert-confirm
+//= require dist/dashboard/jquery.dataTables.min
+//= require dist/dashboard/jquery.dataTables.bootstrap.min
+//= require dashboard/setup
