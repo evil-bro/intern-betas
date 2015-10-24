af8e3f96b9ec952df906c51955897034cae9ade2
unknown <agabdulin@gmail.com>
2015-10-24 22:37:16 +1300

    fixed sorting

--- app/assets/javascripts/dashboard/setup.js
+++ app/assets/javascripts/dashboard/setup.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
-	$('#vacancies-table').DataTable();
+	$('#vacancies-table').DataTable({
+		columnDefs: [ {
+            targets: [ 0 ],
+            orderData: [ 0, 1 ]
+        }, {
+            targets: [ 1 ],
+            orderData: [ 1, 0 ]
+        }, {
+        	targets: [3],
+        	orderable: false
+        }]
+	});
 })
